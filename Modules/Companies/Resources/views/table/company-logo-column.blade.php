a29b3a5315e62a6817ac69cf4c5155a27014902f
Kasper <info@datas.dk>
2026-05-16 00:23:25 +0200

    Fjernet Modules mappen fra GitHub (beholdt lokalt)

--- Modules/Companies/Resources/views/table/company-logo-column.blade.php
+++
@@ -1,7 +0,0 @@
-@if($company->logo)
-    <img src="{{ $company->logo }}" 
-         alt="{{ $company->name ?? 'Logo' }}" 
-         class="w-16 h-16 object-contain rounded border">
-@else
-    -
-@endif
